bc6b90d0d9c00943a022c1cc5cda4c6a5c5bcffd
Jessica Jeng <mingyoungjeng@gmail.com>
2025-05-29 12:52:57 -0500

    Fixed pinned qubits and correct licensing

---
+++ src/sabre/mod.rs
@@ -0,0 +1,45 @@
+// This code is part of Qiskit.
+//
+// (C) Copyright IBM 2022
+// (C) Copyright Jessica Jeng 2025
+//
+// This code is licensed under the Apache License, Version 2.0. You may
+// obtain a copy of this license in the LICENSE.txt file in the root directory
+// of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+//
+// Any modifications or derivative works of this code must retain this
+// copyright notice, and modified files need to carry a notice indicating
+// that they have been altered from the originals.
+//
+// MODIFICATIONS (2025)
+// - Modified module to point to qiskit_accelerate when appropriate and this modified routing module when appropriate.
+
+pub mod route;
+
+// Qiskit made a bunch of stuff private
+pub mod heuristic;
+pub mod layer;
+pub mod neighbor_table;
+
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+pub fn sabre(m: &Bound<PyModule>) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(route::sabre_routing))?;
+    // m.add_wrapped(wrap_pyfunction!(layout::sabre_layout_and_routing))?;
+
+    m.add_class::<heuristic::SetScaling>()?;
+    m.add_class::<heuristic::Heuristic>()?;
+    m.add_class::<heuristic::BasicHeuristic>()?;
+    m.add_class::<heuristic::LookaheadHeuristic>()?;
+    m.add_class::<heuristic::DecayHeuristic>()?;
+    m.add_class::<neighbor_table::NeighborTable>()?;
+    m.add_class::<qiskit_accelerate::sabre::sabre_dag::SabreDAG>()?;
+    // m.add_class::<SwapMap>()?;
+    // m.add_class::<BlockResult>()?;
+    // m.add_class::<NodeBlockResults>()?;
+    // m.add_class::<SabreResult>()?;
+
+    m.add_class::<qiskit_accelerate::nlayout::NLayout>()?;
+    Ok(())
+}
