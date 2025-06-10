afd8a5a7d12817362612e4218ff98a1c6520a60d
alvinquantum <agonzales@anl.gov>
2025-06-10 17:08:00 -0500

    code clean up
    removed from qiskit import * from src files

--- src/quantem/pauli_checks.py
+++ src/quantem/pauli_checks.py
@@ -8,7 +8,10 @@ from typing import Callable
 from collections import defaultdict
 from qiskit.dagcircuit import DAGOutNode, DAGOpNode
 
-from qiskit import *
+from qiskit import (
+    QuantumCircuit,
+    ClassicalRegister
+)
 from typing import List
 from qiskit.converters import circuit_to_dag
 from qiskit.dagcircuit import DAGOpNode
