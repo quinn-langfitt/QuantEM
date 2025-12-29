3101b81e330f1970927f3e61ae9b13c7b8a19d05
Ji Liu <jliu45@ncsu.edu>
2025-12-29 10:01:48 -0600

    update license

--- src/quantem/compiler.py
+++ src/quantem/compiler.py
@@ -1,3 +1,17 @@
+'''Copyright © 2025 UChicago Argonne, LLC and Northwestern University All right reserved
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at:
+
+    https://github.com/quinn-langfitt/QuantEM/blob/main/LICENSE.txt
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.'''
+
 """QuantEM Quantum Error Detection Compiler.
 
 This module provides the main QEDCompiler class for automatically integrating
